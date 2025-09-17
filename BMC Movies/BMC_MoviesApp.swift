c422f84f7dbf964f88f6bc3fa0451923125e2763
Basith P <basithpofficial@gmail.com>
2025-09-17 19:40:49 +0530

    Initial Commit

---
+++ BMC Movies/BMC_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMC_MoviesApp.swift
+//  BMC Movies
+//
+//  Created by Basith P on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BMC_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
